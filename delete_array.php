4612674a1b5faea1ae8eac4867f134810505f008
Willi <willi@skyshi.com>
2014-10-29 15:56:07 +0700

    hapus dan tambah

--- delete_array.php
+++ delete_array.php
@@ -2,12 +2,5 @@
 $array = array("string1", "string2", "string3", "string4", "string3");
 remove_string($array);
 print_r($array);
-
-function remove_string(&$array){
-	$index = array_search('string3',$array);
-	if($index !== FALSE){
-		unset($array[$index]);
-		remove_string($array);
-	}
-}
+// coba coba
 ?>
